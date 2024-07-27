bdf9bfa5801edeea5c1eb99207a8db7f422eefc0
Mateus Farias <mateus.f2001@hotmail.com>
2024-07-27 10:58:13 -0300

    Adcionado novos treinos no Mock Data

---
+++ src/mockData.js
@@ -0,0 +1,83 @@
+// src/mockData.js
+export const treinosData = {
+  "Treino C": {
+    data: "03/06/2024 13:50:41",
+    aluno: "José Carlos da Silva",
+    ultimoTreino: "03/06/2024",
+    exercicios: [
+      {
+        nome: "Mobilidade de Ombro com bastão",
+        series: "2",
+        repeticoes: "10/10",
+        carga: "0",
+        descanso: "0",
+      },
+      {
+        nome: "Rotação interna de ombro na polia",
+        series: "2",
+        repeticoes: "10/10",
+        carga: "5/5",
+        descanso: "5",
+      },
+    ],
+    responsavel: "Raul dos Santos",
+  },
+  "Treino A": {
+    data: "01/06/2024 10:00:00",
+    aluno: "Ana Paula de Souza",
+    ultimoTreino: "01/06/2024",
+    exercicios: [
+      {
+        nome: "Agachamento livre",
+        series: "3",
+        repeticoes: "12",
+        carga: "20kg",
+        descanso: "60",
+      },
+      {
+        nome: "Leg press",
+        series: "3",
+        repeticoes: "15",
+        carga: "100kg",
+        descanso: "60",
+      },
+      {
+        nome: "Cadeira extensora",
+        series: "3",
+        repeticoes: "15",
+        carga: "25kg",
+        descanso: "45",
+      },
+    ],
+    responsavel: "Carlos Lima",
+  },
+  "Treino B": {
+    data: "02/06/2024 15:30:00",
+    aluno: "Ricardo Martins",
+    ultimoTreino: "02/06/2024",
+    exercicios: [
+      {
+        nome: "Supino reto",
+        series: "4",
+        repeticoes: "8",
+        carga: "80kg",
+        descanso: "90",
+      },
+      {
+        nome: "Desenvolvimento com halteres",
+        series: "4",
+        repeticoes: "10",
+        carga: "15kg",
+        descanso: "60",
+      },
+      {
+        nome: "Crucifixo inclinado",
+        series: "3",
+        repeticoes: "12",
+        carga: "12kg",
+        descanso: "45",
+      },
+    ],
+    responsavel: "Mariana Almeida",
+  },
+};
